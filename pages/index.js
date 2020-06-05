ecfca682d8f310b7fd11ca10770f71012c00b801
wwarodom <wwarodom@gmail.com>
2020-06-05 23:03:41 +0700

    Add card ui

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
-import Layout from '../components/Layout' 
+import Layout from '../components/Layout'
 import { useSelector } from 'react-redux'
-import withAuth from '../hoc/auth' 
+import withAuth from '../hoc/auth'
+import {Card} from 'antd'
 
 function Home (props) {
-  let user = useSelector(state => state.user.user) 
+  let user = useSelector(state => state.user.user)
   user = user || props.user
   return (
     <Layout menuId='1' user={user}>
-      <div className='topBox'> Home</div> 
+      <div>
+        <div >
+          <h1>Todo</h1>
+        </div>
+        <div>
+          <Card >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
+      </div>
     </Layout>
   )
-} 
+}
 
 export default withAuth(Home)
